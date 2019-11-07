52fdfe68e094b2d2975985695fa590e16b737c28
xomyachok_shaolin <xomyackok8@gmail.com>
2019-11-07 17:47:10 +0500

    curves0

--- curves/curves.cpp
+++ curves/curves.cpp
@@ -2,11 +2,34 @@
 /* curves.cpp */
 #include "curves.h"
 
-//
+// размер графика
+#define GX (double)7
+#define GY (double)2
+// шаг по X
+#define DX 5
+
+// размер устр-ва вывода
+int WX = 0, WY = 0;
+// масштаб осей
+double CX = 0, CY = 0;
+
+/* параметры вывода */
+void setview() {
+	RECT rc;
+	GetClientRect(hWndGDE, &rc);
+	WX = rc.right - rc.left;
+	WY = rc.bottom - rc.top;
+	// Центр системы координат
+	CX = 0; CY = WY / 2;
+	CX = 0; CY = WY / 2;
+}
 
 /* контекст устройства GDC */
 void draw() {
-    MoveToEx(GDC, 300, 0, 0);
-    LineTo(GDC, 0, 0);
-    LineTo(GDC, 0, 300);
+	setview();
+	// Оси координат
+	MoveToEx(GDC, 0, CY, NULL);
+	LineTo(GDC, WX, CY);
+	MoveToEx(GDC, CX, 0, NULL);
+	LineTo(GDC, CX, WY);
 }
